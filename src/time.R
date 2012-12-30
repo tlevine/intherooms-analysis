fae5a2930c12f143432a69e20d2ad0cd027967c3
Thomas Levine <occurrence@thomaslevine.com>
2012-12-30 00:49:31 -0500

    aoeu

--- src/time.R
+++ src/time.R
@@ -5,24 +5,27 @@ PRETTY.TIME$time <- rep(paste(rep(0:23, each = 2), rep(c('00', '30'), 24), sep =
 row.names(PRETTY.TIME) <- PRETTY.TIME$ugly
 PRETTY.TIME$ugly <- NULL
 
-.base <- map.base + xlim(-74.2, -73.8) + ylim(40.4, 40.8)
 plot.hour.of.week <- function(hour) {
     pretty.time <- paste(PRETTY.TIME[as.character(hour),], collapse = ' at ')
     m <- current.meetings(na.omit(meeting), hour)
+    .base <- ggplot(m) + xlim(-74.05, -73.8) + ylim(40.6, 40.8) +
+        coord_map('ortho', orientation=c(41, -74, 0)) +
+        theme(legend.position = "none") +
+        # scale_colour_discrete(drop = FALSE) +
+        aes(alpha = 0.5, size = 0) +
+        labs(title = pretty.time) +
+        geom_point()
     if (nrow(m) > 0) {
-        .base +
-        geom_point(data = m, aes(x = Longitude, y = Latitude, opacity = 0.2, color = Fellowship)) +
-        scale_colour_discrete(drop = FALSE) +
-        labs(title = pretty.time)
+        .base + aes(x = Longitude, y = Latitude, opacity = 0.2, color = Fellowship)
     } else {
         paste('No meetings for', pretty.time)
-        .base
+        .base + aes(x = 0, y = 0)
     }
 }
 
 pdf('graphs/over.week.pdf', width = 11, height = 8.5)
-# for (i in 1:length(hours.of.week)) {
-for (i in 154:157) {
+for (i in 1:length(hours.of.week)) {
+# for (i in 130:140) {
     print(plot.hour.of.week(hours.of.week[i]))
 }
 dev.off()
