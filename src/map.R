fcfa774cabf83bd8b39bc659b8dbeb13d30b75a3
Thomas Levine <occurrence@thomaslevine.com>
2012-12-30 17:06:18 -0500

    aoeu

--- src/map.R
+++ src/map.R
@@ -1,5 +1,16 @@
-p.all <- map.base + geom_point(data = meeting, aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship))
+p.all <- map.base + geom_point(data = meeting, aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship)) +
+  xlab('Longitude') + ylab('Latitude')
 
 p.cropped <- ggplot(meeting) +
   aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship) +
-  xlim(-74.5, -73.5) + ylim(40.5, 41) + geom_point()
+  xlim(-74.05, -73.8) + ylim(40.6, 40.8) +
+  geom_point()
+
+png('graphs/map.png', width=1260, height = 800)
+#png('graphs/map.png', width=840, height = 500)
+print(p.all + theme_gray(18))
+dev.off()
+
+png('graphs/map-cropped.png', width=1260, height = 800)
+print(p.cropped + theme_gray(18))
+dev.off()
