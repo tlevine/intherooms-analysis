3e3358d0cdfc6307570fcfab87daae5e706294fd
Thomas Levine <occurrence@thomaslevine.com>
2013-02-28 18:53:14 -0500

    aoeu

--- lib/map.R
+++
@@ -1,6 +0,0 @@
-nyc <- map_data('state', region = c('new york', 'new jersey', 'connecticut', 'pennsylvania'))
-usa <- map_data('state')
-
-map.base <- ggplot() +
-  geom_polygon(data = usa, aes(x = long, y = lat, group = group), colour = "white", fill = "grey10") +
-  coord_map('ortho', orientation=c(38, -98, 0))
--- src/map.R
+++ src/map.R
@@ -1,3 +1,9 @@
+usa <- map_data('state')
+
+map.base <- ggplot() +
+  geom_polygon(data = usa, aes(x = long, y = lat, group = group), colour = "white", fill = "grey10") +
+  coord_map('ortho', orientation=c(38, -98, 0))
+
 #p <- map.base + geom_point(data = location, aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship)) +
 p <- map.base + geom_point(data = location, aes(x = Longitude, y = Latitude, opacity = 0.1)) +
   xlab('Longitude') + ylab('Latitude')
