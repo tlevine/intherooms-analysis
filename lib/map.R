0c2d489efdedf17e66362f8678aade5a3b67a4ad
Thomas Levine <occurrence@thomaslevine.com>
2013-02-28 18:27:28 -0500

    oeu

--- lib/map.R
+++ lib/map.R
@@ -1,6 +1,6 @@
-library(ggplot2)
 nyc <- map_data('state', region = c('new york', 'new jersey', 'connecticut', 'pennsylvania'))
+usa <- map_data('state')
 
 map.base <- ggplot() +
-  geom_polygon(data = nyc, aes(x = long, y = lat, group = group), colour = "white", fill = "grey10") +
-  coord_map('ortho', orientation=c(41, -74, 0))
+  geom_polygon(data = usa, aes(x = long, y = lat, group = group), colour = "white", fill = "grey10") +
+  coord_map('ortho', orientation=c(38, -98, 0))
--- src/map.R
+++ src/map.R
@@ -1,18 +1,7 @@
-p.all <- map.base + geom_point(data = meeting, aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship)) +
+#p <- map.base + geom_point(data = location, aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship)) +
+p <- map.base + geom_point(data = location, aes(x = Longitude, y = Latitude, opacity = 0.1)) +
   xlab('Longitude') + ylab('Latitude')
 
-p.cropped <- ggplot(meeting) +
-  aes(x = Longitude, y = Latitude, opacity = 0.1, color = Fellowship) +
-  xlim(-74.05, -73.8) + ylim(40.6, 40.8) +
-  geom_point()
-
-png('graphs/map.png', width=1260, height = 800)
-#png('graphs/map.png', width=840, height = 500)
-print(p.all)
-dev.off()
-
-png('graphs/map-cropped.png', width=1260, height = 800)
-print(p.cropped + theme_gray(18))
-dev.off()
-
-
+#png('graphs/map.png', width=1260, height = 800)
+#print(p.all)
+#dev.off()
